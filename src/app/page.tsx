6e34e5252bff64bd81976fba95ee51ff7c351bc4
willcruz07 <lynkan@hotmail.com>
2024-06-04 15:34:35 -0300

    adjust responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1.8, delay: 1 }}
-        className="relative h-48 w-64 sm:h-72 sm:w-96"
+        className="relative h-40 w-56 sm:h-72 sm:w-96"
       >
         <Image
           src="/Logo-2.png"
@@ -25,7 +25,7 @@ export default function Home() {
         initial={{opacity: 0}}
         animate={{opacity: 1}} 
         transition={{duration: 1.8, delay: .5}}
-        className="max-w-6xl text-center text-md text-neutral-400 antialiased tracking-wide leading-7 font-sans p-4 mt-4"
+        className="max-w-6xl text-center text-sm md:text-lg sm:text-lg text-neutral-400 antialiased tracking-wide  leading-6 md:leading-7 lg:leading-7 font-sans p-4 mt-2 md:mt-8"
       >
         {`"A Will Dev é uma equipe de especialistas apaixonados por tecnologia e inovação, dedicados a transformar ideias em realidade digital. Nossa empresa oferece serviços de desenvolvimento de software personalizado, adaptados às necessidades exclusivas de cada cliente. Com uma abordagem centrada no cliente e uma vasta experiência em diversas plataformas e tecnologias, ajudamos empresas de todos os tamanhos a criar soluções de software robustas, intuitivas e escaláveis. Desde o conceito inicial até a implementação e manutenção contínua, estamos comprometidos em fornecer soluções de software de alta qualidade que impulsionam o sucesso de nossos clientes no mercado digital."`}
       </motion.span>  
